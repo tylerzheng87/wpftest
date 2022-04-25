6e49c20b6e84ebaba3dc0883c1d4f8baf6b9741b
Tyler Wang <15114565716@163.com>
2022-04-25 17:09:26 +0800

    login page

---
+++ WpfApp3/AppData.cs
@@ -0,0 +1,16 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// 全局数据类
+    /// </summary>
+    public class AppData:ViewModelBase
+    {
+    }
+}
---
+++ WpfApp3/Converter/HeightToCornerConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApp3.Converter
+{
+    /// <summary>
+    /// 将高度值分成一半
+    /// </summary>
+    public class HeightToCornerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return value;
+            if (double.TryParse(value.ToString(), out double height))
+            {
+                return height / 2;
+            }
+            else
+            {
+                return value;
+            }
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
